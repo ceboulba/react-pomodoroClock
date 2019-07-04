ed8bbccbd7eb9284381a32793920204b3eb4ae27
ceboulba <antoine@archipicture.fr>
2019-07-04 09:07:49 +0200

    1 file modified

--- Chrono.js
+++ Chrono.js
@@ -3,12 +3,18 @@ import React, { Fragment } from 'react'
 export default ({time}) => {
   const minutes = Math.floor( time / 60 )
   const seconds = time % 60 
-        {console.log('minute : '+ minutes.toString())}
+  console.log('minutes : '+ minutes)
+  const timeOut = (m, s)=>{
+    let mm = m.toString().length > 1 ? `${m}`: `${m}`
+    let ss = s.toString().length > 1 ? `${s}`: `${s}`
+    return `${mm}:${ss}` 
+  }
+
 
     return (
       <Fragment>      
-      time : {time}<br/>
-      <p className="title has-text-centered" id="time-left">{minutes.toString().length > 1 ? minutes:'0'+minutes.toString()}:{seconds.toString().length > 1 ? seconds:'0'+seconds.toString()}</p>
+      time : {time}<br/>      
+      <p className="title has-text-centered" id="time-left">{timeOut(minutes, seconds)}</p>
       </Fragment>
     )
 }
